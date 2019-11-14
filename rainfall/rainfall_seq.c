a0a5f863f1f09f433bc4e8514378789d75be8f0d
Prathikshaa <pr109@duke.edu>
2019-11-14 12:46:54 -0500

    Fleshed out some more details in seq program. Also added gitignore

--- rainfall/rainfall_seq.c
+++ rainfall/rainfall_seq.c
@@ -5,17 +5,21 @@
 
 struct simulation_struct
 {
-	int P;
-	int M;
-	float A;
-	int N;
-	const char * elevation_file;
+	int P; // num_threads
+	int M; // rain steps
+	int num_steps; // total simulation steps
+	float A; // absorption
+	int N; // landscape size
+	int **landscape; // landscape array - input
+	float **track_rain;
+	float **rain_absorbed; // output
+	const char *elevation_file; // name of input file
 } typedef simulation;
 
 void usage(const char *prog_name){
 	printf("Usage: %s <P> <M> <A> <N> <elevation_file> \n", prog_name);
 	printf("-------------------------------------------------------------\n");
-	printf("* P = # of parallel threads to use. \n");
+	printf("* P = # of parallel threads to use (Ignored in this version of the code). \n");
 	printf("* M = # of simulation time steps during which a rain drop will fall"
 		" on each landscape point. In other words, 1 rain drop falls on each"
 		" point during the first M steps of the simulation. \n");
@@ -38,7 +42,7 @@ size_t str_to_num(const char *str) {
     exit(EXIT_FAILURE);
   }
   errno = 0; // reset errno before call
-  size_t val = strtoul(str, &endptr, 10);
+  size_t val = strtoul(str, &endptr, 10); // convert to num
 
   if (errno) {
     perror("str_to_num: Invalid Input: ");
@@ -57,7 +61,7 @@ size_t str_to_num(const char *str) {
 float str_to_float(const char *str){
 	char *endptr;
 	errno = 0; // reset errno before call
-	float val = strtof(str, &endptr);
+	float val = strtof(str, &endptr); // convert to float
 	if((endptr == NULL)||(errno)){
 		fprintf(stderr, "str_to_float: Invalid Input: %s\n", str);
     	exit(EXIT_FAILURE);
@@ -65,24 +69,37 @@ float str_to_float(const char *str){
 	return val;
 }
 
-void read_landscape(int ***landscape_ptr, int N, const char* elevation_file){
-	int ** landscape = *landscape_ptr; // recheck this syntax 
+// read landscape from elevation file
+void read_landscape(simulation *sim_data){
+	// open file sim_data->elevation_file
 	// read N lines
 
 	// for each line, getc non-space character
 	// landscape[i][j] = str_to_num(char)
 }
 
-void run_simulation(simulation* sim_data){
-	int num_threads = sim_data->P;
-	int num_steps = sim_data->M;
+// only function that is parallelized later
+void run_simulation(simulation *sim_data){
+	int num_rain_steps = sim_data->M;
 	float absorption = sim_data->A;
 	int N = sim_data->N;
-	const char *elevation_file = sim_data->elevation_file;
 
-	// create a 2D landscape array
-	int **landscape = malloc(sizeof(int)*N*N);
-	read_landscape(&landscape, N, elevation_file);
+	// loop over num_steps
+	for(int i = 0; i<N; i++){
+	  for(int j = 0; j<N; j++){
+	    // absorb drops in current block
+
+	    // check neighbours to flow the rest
+	    // check i+1, j+1
+	    
+	  }
+	}
+}
+
+// write the result of the simulation to output file
+void write_result(simulation *sim_data){
+	// write sim_data->rain_absorbed to elevation_file + ".out"
+	// space seperated
 }
 
 int main(int argc, char const *argv[])
@@ -94,13 +111,26 @@ int main(int argc, char const *argv[])
 	}
 	simulation *sim_data = malloc(sizeof(simulation));
 	sim_data->P = str_to_num(argv[1]); // num_threads = P
-	sim_data->M = str_to_num(argv[2]); // num_steps = M
+	sim_data->M = str_to_num(argv[2]); // num_rain_steps = M
 	sim_data->A = str_to_float(argv[3]); // absorption = A
 	sim_data->N = str_to_num(argv[4]); // N dimensional landscape
 	sim_data->elevation_file = argv[5]; // elevation filename
 
-	run_simulation(sim_data);
+	sim_data->num_steps = 0;
+	size_t size_landcape = sizeof(int)*(sim_data->N)*(sim_data->N);
+	sim_data->landscape = malloc(size_landcape);
+	memset(sim_data->landscape, 0, size_landcape);
 
+	size_t size_absorbed = sizeof(float)*(sim_data->N)*(sim_data->N);
+	sim_data->rain_absorbed = malloc(size_absorbed);
+	memset(sim_data->rain_absorbed, 0, size_absorbed);
+
+	read_landscape(sim_data);
+	run_simulation(sim_data);
+	write_result(sim_data);
+
+	free(sim_data->rain_absorbed);
+	free(sim_data->landscape);
 	free(sim_data);
 	return EXIT_SUCCESS;
 }
